448832c7499e9045bbc880ea038a28a3e597bbc0
devian5 <acgarage@outlook.es>
2021-02-04 23:53:22 +0100

    I have created the logic of attacking and centered players of the second screen

--- js/fighter.js
+++ js/fighter.js
@@ -5,26 +5,25 @@ class Fighter{
         this.strength = strength;
         this.defense = defense;
         this.agility = agility;
-        this.luck = luck;
+        this.luck = luck ;
     }
 
     attack(rival){
-        rival.health -= (this.strength - this.defense) * this.fluke() * this.dexterity();
-        console.log(rival.health)
+        rival.health -= (this.strength - this.defense);
     }
 
     specialAttack(rival){
-        rival.health -= (this.strength * 0.2) - this.defense; 
+        rival.health -= (this.strength * 0.2 + this.strength) - this.defense; 
 
     }
 
-    fluke(player){
-        this.random(2,player.luck)
-    }
+    // fluke(player){
+    //     this.random(2,player.luck)
+    // }
 
-    dexterity(player){
-        this.random(5, player.agility)
-    }
+    // dexterity(player){
+    //     this.random(5, player.agility)
+    // }
 
     
 
@@ -75,6 +74,7 @@ let selectFighter = (fighter) => {
                 document.getElementById(teamA[i])
             };
         };
+        team1Assignment();
         document.getElementById(fighter).onclick = ''; 
     }else if(teamB.length < 3){
         //lo introducimos en la B
@@ -82,21 +82,22 @@ let selectFighter = (fighter) => {
 
         let elementB = document.getElementById(fighter)
         elementB.classList.add('playerB');
-        document.getElementById(fighter).onclick = ''; 
+        document.getElementById(fighter).onclick = '';
+        team2Assignment(); 
 
         if(teamB.length == 3){
             let showTeamA = document.getElementById('squadA');
             let showTeamB = document.getElementById('squadB');
         
             showTeamA.innerHTML = `
-                <div><img class="pepeStile" src="img/${teamA[0].nick}.gif"></div>
-                <div><img class="pepeStile" src="img/${teamA[1].nick}.gif"></div>
-                <div><img class="pepeStile" src="img/${teamA[2].nick}.gif"></div>
+                <div><img class="pepeStyle" src="img/${teamA[0].nick}.gif"></div>
+                <div><img class="pepeStyle" src="img/${teamA[1].nick}.gif"></div>
+                <div><img class="pepeStyle" src="img/${teamA[2].nick}.gif"></div>
             `;
             showTeamB.innerHTML = `
-                <div><img class="pepeStile" src="img/${teamB[0].nick}.gif"></div> 
-                <div><img class="pepeStile" src="img/${teamB[1].nick}.gif"></div> 
-                <div><img class="pepeStile" src="img/${teamB[2].nick}.gif"></div>
+                <div><img class="pepeStyle" src="img/${teamB[0].nick}.gif"></div> 
+                <div><img class="pepeStyle" src="img/${teamB[1].nick}.gif"></div> 
+                <div><img class="pepeStyle" src="img/${teamB[2].nick}.gif"></div>
                 `;
            
             resolveIn(2000).then(delay => {
@@ -107,9 +108,25 @@ let selectFighter = (fighter) => {
             console.log(showTeamA.innerHTML)
         }
 
-        
     }
+    console.log(p1,p2,p3,p4,p5,p6)  
 }
+
+let team1Assignment = () =>{
+    for(let i = 0; i< teamA.length; i++){
+        p1 = teamA[0];
+        p2 = teamA[1];
+        p3 = teamA[2];
+    };
+};
+let team2Assignment = () =>{
+    for(let i = 0; i< teamB.length; i++){
+        p4 = teamB[0];
+        p5 = teamB[1];
+        p6 = teamB[2];
+    };
+};
+
 
 const resolveIn = delay =>
 new Promise(res => setTimeout(() => res(delay), delay));
@@ -128,27 +145,34 @@ let hit = () => {
     let turn = Math.floor(Math.random() * 2);
     let special = Math.floor(Math.random() * 5);
 
-    console.log(teamA[0])
     console.log('IEEEEEEEEEEEEE')
     
     if(turn == 0){
         if(special == 3){
-            console.log('AL ATAAAAQUEEEE!')
-            teamA[0].nick.specialAttack(teamB[0].nick);
+            console.log('ATAAAAQUEEEE!')
+            p1.specialAttack(p4);
+            p2.specialAttack(p5);
+            p3.specialAttack(p6);
         }else{
-            console.log('ATAQUE xD')
-            teamA[0].nick.attack(teamB[0].nick);
+            console.log('SPECIAL')
+            p1.attack(p4);
+            p2.attack(p5);
+            p3.attack(p6);
         }
     }else{
         if(special == 3){
-            teamB[0].nick.specialAttack(teamA[0].nick);
+            p4.specialAttack(p1);
+            p5.specialAttack(p2);
+            p6.specialAttack(p3);
         }else{
-            teamB[0].nick.attack(teamA[0].nick);
+            p4.attack(p1);
+            p5.attack(p2);
+            p6.attack(p3);
         }
-
     }
 
-    console.log(teamA[0].nick, teamA[0].health);
-    console.log(teamB[0].nick, teamB[0].health);
+    console.log(p1.nick, ':', p1.health);
+    console.log(p2.nick, ':', p2.health);
 
 }
+
