4fd836e49dddb48fdbe6c1c1b3a24c90d838f27e
devian5 <acgarage@outlook.es>
2021-02-04 19:22:47 +0100

    I have created the logic of players selected on the second screen

--- js/fighter.js
+++ js/fighter.js
@@ -34,17 +34,14 @@ class Fighter{
 
 }
 
-export default Fighter
-
-
-// nick,health,strength,defense,agility,luck
 
 let player1 = new Fighter('pikachu',200,120,100,150,8)
-let player2 = new Fighter('mario',150,100,120,150,9)
-let player3 = new Fighter('sonic',150,150,90,200,7)
-let player4 = new Fighter('wario',140,170,170,120,7)
-let player5 = new Fighter('browser',150,180,180,100,4)
-let player6 = new Fighter('link',150,180,150,180,8)
+let player2 = new Fighter('mario',200,100,120,150,9)
+let player3 = new Fighter('sonic',200,150,90,200,7)
+let player4 = new Fighter('wario',200,170,170,120,7)
+let player5 = new Fighter('browser',200,180,180,100,4)
+let player6 = new Fighter('link',200,180,150,180,8)
+
 
 let p1 = '';
 let p2 = '';
@@ -52,3 +49,103 @@ let p3 = '';
 let p4 = '';
 let p5 = '';
 let p6 = '';
+
+let allPlayers = {
+    'pikachu': player1,
+    'mario': player2,
+    'sonic': player3,
+    'wario': player4,
+    'browser': player5,
+    'link': player6
+};
+
+
+let teamA = [];
+let teamB = [];
+
+        
+let selectFighter = (fighter) => {
+
+    if(teamA.length < 3){
+        teamA.push(allPlayers[fighter])
+        let elementA = document.getElementById(fighter);
+        elementA.classList.add('playerA');
+        if(teamA.length == 3){
+            for(let i = 0; i < teamA.length; i++){
+                document.getElementById(teamA[i])
+            };
+        };
+        document.getElementById(fighter).onclick = ''; 
+    }else if(teamB.length < 3){
+        //lo introducimos en la B
+        teamB.push(allPlayers[fighter])
+
+        let elementB = document.getElementById(fighter)
+        elementB.classList.add('playerB');
+        document.getElementById(fighter).onclick = ''; 
+
+        if(teamB.length == 3){
+            let showTeamA = document.getElementById('squadA');
+            let showTeamB = document.getElementById('squadB');
+        
+            showTeamA.innerHTML = `
+                <div><img class="pepeStile" src="img/${teamA[0].nick}.gif"></div>
+                <div><img class="pepeStile" src="img/${teamA[1].nick}.gif"></div>
+                <div><img class="pepeStile" src="img/${teamA[2].nick}.gif"></div>
+            `;
+            showTeamB.innerHTML = `
+                <div><img class="pepeStile" src="img/${teamB[0].nick}.gif"></div> 
+                <div><img class="pepeStile" src="img/${teamB[1].nick}.gif"></div> 
+                <div><img class="pepeStile" src="img/${teamB[2].nick}.gif"></div>
+                `;
+           
+            resolveIn(2000).then(delay => {
+                
+                screenChange('screen1', 'screen2');
+                
+            });
+        }
+
+        
+    }
+}
+
+const resolveIn = delay =>
+new Promise(res => setTimeout(() => res(delay), delay));
+
+let screenChange = (present,future) =>{
+    let screenPresent = document.getElementById(present);
+    let screenFuture = document.getElementById(future);
+
+    screenPresent.style.display = 'none';
+    screenFuture.style.display = 'block'
+}
+
+
+
+let hit = () => {
+    let turn = Math.floor(Math.random() * 2);
+    let special = Math.floor(Math.random() * 5);
+
+    
+    if(turn == 0){
+        if(special == 3){
+            console.log('AL ATAAAAQUEEEE!')
+            teamA[0].specialAttack(teamB[0]);
+        }else{
+            console.log('ATAQUE xD')
+            teamA[0].attack(teamB[0]);
+        }
+    }else{
+        if(special == 3){
+            teamA[0].specialAttack(teamB[0]);
+        }else{
+            teamB[0].attack(teamB[0]);
+        }
+
+    }
+
+    console.log(teamA.nick, teamA.health);
+    console.log(teamB.nick, teamB.health);
+
+}
--- js/initialGame.js
+++ js/initialGame.js
@@ -1,70 +1,86 @@
 
-let teamA = [];
-let teamB = [];
+// let teamA = [];
+// let teamB = [];
 
-let openingGame = {
-    
-    players(){
-        let player1 = new Fighter('pikachu',200,120,100,150,8)
-        let player2 = new Fighter('mario',150,100,120,150,9)
-        let player3 = new Fighter('sonic',150,150,90,200,7)
-        let player4 = new Fighter('wario',140,170,170,120,7)
-        let player5 = new Fighter('browser',150,180,180,100,4)
-        let player6 = new Fighter('link',150,180,150,180,8)
-        
-        let p1 = '';
-        let p2 = '';
-        let p3 = '';
-        let p4 = '';
-        let p5 = '';
-        let p6 = '';
-    },
-    
-    // startGame(){
-        
-    //     let initialHealthP1 = 200;
-    //     let initialHealthP2 = 150;
-    //     let initialHealthP3 = 150;
-    //     let initialHealthP4 = 140;
-    //     let initialHealthP5 = 150;
-    //     let initialHealthP6 = 150;
-        
-    //     let initialHealthP1 = player1.health
-        
-    //     let p1 = '';
-    //     let p2 = '';
-    //     let p3 = '';
-    //     let p4 = '';
-    //     let p5 = '';
-    //     let p6 = '';
-        
-    // },
-    
-    selectFighter(fighter){
-        if(teamA.length < 3){
-            teamA.push(fighter);
-            let elementA = document.getElementById(fighter);
-            elementA.classList.add('playerA');
-            if(teamA.length == 3){
-                for(let i = 0; i < teamA.length; i++){
-                    document.getElementById(teamA[i])
-                };
-            };
-            document.getElementById(fighter).onclick = ''; 
-        }else if(teamB.length < 3){
-            //lo introducimos en la B
-            teamB.push(fighter);
-            let elementB = document.getElementById(fighter)
-            elementB.classList.add('playerB');
-            document.getElementById(fighter).onclick = ''; 
-        }
+// let player1 = new Fighter('pikachu',200,120,100,150,8)
+// let player2 = new Fighter('mario',200,100,120,150,9)
+// let player3 = new Fighter('sonic',200,150,90,200,7)
+// let player4 = new Fighter('wario',200,170,170,120,7)
+// let player5 = new Fighter('browser',200,180,180,100,4)
+// let player6 = new Fighter('link',200,180,150,180,8)
 
-        console.log('Esto es teamA', teamA);
-        console.log('Esto es teamB', teamB);
-    
-    
-    }
 
-}
+// let p1 = '';
+// let p2 = '';
+// let p3 = '';
+// let p4 = '';
+// let p5 = '';
+// let p6 = '';
+
+// let allPlayers = {
+//     'pikachu': player1,
+//     'mario': player2,
+//     'sonic': player3,
+//     'wario': player4,
+//     'browser': player5,
+//     'link': player6
+// };
+
+
+// let openingGame = {
+    
+//     players(){
+        
+//     },    
+    
+    
+    
+//     // startGame(){
+        
+//         //     let initialHealthP1 = 200;
+        
+//         //     let initialHealthP1 = player1.health
+    
+//         //     let p1 = '';
+    
+//     // },    
+    
+//     selectFighter(fighter){
+
+//         if(teamA.length < 3){
+//             // teamA.push(fighter);
+//             teamA.push(allPlayers[fighter])
+//             let elementA = document.getElementById(fighter);
+//             elementA.classList.add('playerA');
+//             if(teamA.length == 3){
+//                 for(let i = 0; i < teamA.length; i++){
+//                     document.getElementById(teamA[i])
+//                 };
+//             };
+//             document.getElementById(fighter).onclick = ''; 
+//         }else if(teamB.length < 3){
+//             //lo introducimos en la B
+//             // teamB.push(fighter);
+//             teamB.push(allPlayers[fighter])
+
+//             let elementB = document.getElementById(fighter)
+//             elementB.classList.add('playerB');
+//             document.getElementById(fighter).onclick = ''; 
+//         }
+
+//         // if(teamA.length == 2 && teamB.length == 2){
+//         //     for(let i = 1; i < 4; i++){
+//         //         player+i
+//         //         allPlayers[teamA[i]]
+//         //     }
+//         // }
+
+//         console.log('Esto es teamA', teamA);
+//         console.log('Esto es teamB', teamB);
+    
+    
+//     }
+
+// }
 
 
