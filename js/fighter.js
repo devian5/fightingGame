d6a52585b4c89f78adadefeb2c964a4a8aef2348
devian5 <acgarage@outlook.es>
2021-02-06 00:14:55 +0100

    I have corrected the life bar with all players

--- js/fighter.js
+++ js/fighter.js
@@ -27,10 +27,10 @@ class Fighter{
 // nick,health,strength,defense,agility,luck
 
 let player1 = new Fighter('pikachu',200,20,10,50,3)
-let player2 = new Fighter('mario',200,15,12,50,3)
+let player2 = new Fighter('mario',200,20,10,50,3)
 let player3 = new Fighter('sonic',200,25,10,60,2)
 let player4 = new Fighter('wario',200,35,50,20,2)
-let player5 = new Fighter('browser',200,40,60,15,2)
+let player5 = new Fighter('browser',200,40,40,15,3)
 let player6 = new Fighter('link',200,40,50,50,3)
 
 
@@ -135,10 +135,14 @@ let screenChange = (present,future) =>{
 
 
 let hit = () => {
-    let turn = Math.floor(Math.random() * 3);
+    let turn = Math.floor(Math.random() * 2);
     let special = Math.floor(Math.random() * 2);
     let healthPlayer1 = document.getElementById("healthPlayer1");
     let healthPlayer2 = document.getElementById("healthPlayer2");
+    let healthPlayer3 = document.getElementById("healthPlayer3");
+    let healthPlayer4 = document.getElementById("healthPlayer4");
+    let healthPlayer5 = document.getElementById("healthPlayer5");
+    let healthPlayer6 = document.getElementById("healthPlayer6");
 
     if(turn == 0){
         if(special == 3){
@@ -147,8 +151,8 @@ let hit = () => {
             p2.specialAttack(p5);
             p3.specialAttack(p6);
             healthPlayer1.value = `${p1.health}`  
-            healthPlayer2.value = `${p1.health}`  
-            healthPlayer3.value = `${p1.health}`  
+            healthPlayer2.value = `${p2.health}`  
+            healthPlayer3.value = `${p3.health}`  
 
         }else{
             
@@ -157,28 +161,36 @@ let hit = () => {
             p2.attack(p5);
             p3.attack(p6);
             healthPlayer1.value = `${p1.health}`  
-            healthPlayer2.value = `${p1.health}`  
-            healthPlayer3.value = `${p1.health}`  
+            healthPlayer2.value = `${p2.health}`  
+            healthPlayer3.value = `${p3.health}`  
         }
     }else{
         if(special == 3){
             p4.specialAttack(p1);
             p5.specialAttack(p2);
             p6.specialAttack(p3);
-            healthPlayer4.value = `${p1.health}`  
-            healthPlayer5.value = `${p1.health}`  
-            healthPlayer6.value = `${p1.health}`  
+            healthPlayer4.value = `${p4.health}`  
+            healthPlayer5.value = `${p5.health}`  
+            healthPlayer6.value = `${p6.health}`  
             
         }else{
             p4.attack(p1);
             p5.attack(p2);
             p6.attack(p3);
-            healthPlayer4.value = `${p1.health}`  
-            healthPlayer5.value = `${p1.health}`  
-            healthPlayer6.value = `${p1.health}`  
+            healthPlayer4.value = `${p4.health}`  
+            healthPlayer5.value = `${p5.health}`  
+            healthPlayer6.value = `${p6.health}`  
         }
 
     }
+
+    // winner = document.getElementById('winner')
+
+    // if(p1.health < 0){
+    //     winner.innerHTML = `${p4.nick} is win!`
+    // }else{
+    //     winner.innerHTML = `${p1.nick} is win`
+    // }
     console.log('VIDA Player1', healthPlayer1)  
     console.log('VIDA Player2', healthPlayer2)  
     console.log('VIDA Player3', healthPlayer3)  
@@ -189,15 +201,15 @@ let hit = () => {
     console.log('PRIMERA',p1.nick, ':', Math.floor(p1.health));
     console.log('<------------------------------------->')
     console.log('SEGUNDA',p2.nick, ':', Math.floor(p2.health));
-    // console.log('<------------------------------------->')
-    // console.log('TERCERA',p3.nick, ':', Math.floor(p3.health));
-    // console.log('<------------------------------------->')
-    // console.log('CUERTA',p4.nick, ':', Math.floor(p4.health));
-    // console.log('<------------------------------------->')
-    // console.log('QUINTA',p5.nick, ':', Math.floor(p5.health));
-    // console.log('<------------------------------------->')
-    // console.log('SEXTA',p6.nick, ':', Math.floor(p6.health));
-    // console.log('SIGUIENTE RONDA')
+    console.log('<------------------------------------->')
+    console.log('TERCERA',p3.nick, ':', Math.floor(p3.health));
+    console.log('<------------------------------------->')
+    console.log('CUERTA',p4.nick, ':', Math.floor(p4.health));
+    console.log('<------------------------------------->')
+    console.log('QUINTA',p5.nick, ':', Math.floor(p5.health));
+    console.log('<------------------------------------->')
+    console.log('SEXTA',p6.nick, ':', Math.floor(p6.health));
+    console.log('SIGUIENTE RONDA')
 
 }
 
