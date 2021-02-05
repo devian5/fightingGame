9fe655e6160335f5f1232a7155609c251f42d988
devian5 <acgarage@outlook.es>
2021-02-05 22:43:29 +0100

    I have created the life bar for each player along with their logic

--- js/fighter.js
+++ js/fighter.js
@@ -10,35 +10,28 @@ class Fighter{
 
     attack(rival){
         const getRandomArbitrary = (min, max) => {
-            return Math.random() * (max - min) + min;
+            return Math.floor(Math.random()) * (max - min) + min;
           }
     
-        let fluke = getRandomArbitrary(1.5,rival.luck);
-        rival.health -= (this.strength - this.defense) * fluke;
+        let fluke = getRandomArbitrary(1,rival.luck);
+        rival.health -= (this.strength - this.defense/1.5) * fluke;
     }
 
     specialAttack(rival){
-        rival.health -= (this.strength * 0.2 + this.strength) - this.defense; 
+        rival.health -= (this.strength + this.agility) - this.defense; 
 
     }
 
-    // fluke(){
-    //     this.random(2,this.luck);
-    // }
-
-    // dexterity(player){
-    //     this.random(5, player.agility)
-    // }
-
 }
 
+// nick,health,strength,defense,agility,luck
 
-let player1 = new Fighter('pikachu',200,120,100,150,8)
-let player2 = new Fighter('mario',200,100,120,150,9)
-let player3 = new Fighter('sonic',200,150,90,200,7)
-let player4 = new Fighter('wario',200,170,170,120,7)
-let player5 = new Fighter('browser',200,180,180,100,4)
-let player6 = new Fighter('link',200,180,150,180,8)
+let player1 = new Fighter('pikachu',200,20,10,50,3)
+let player2 = new Fighter('mario',200,15,12,50,3)
+let player3 = new Fighter('sonic',200,25,10,60,2)
+let player4 = new Fighter('wario',200,35,50,20,2)
+let player5 = new Fighter('browser',200,40,60,15,2)
+let player6 = new Fighter('link',200,40,50,50,3)
 
 
 let p1 = '';
@@ -65,23 +58,25 @@ let teamB = [];
 let selectFighter = (fighter) => {
 
     if(teamA.length < 3){
-        teamA.push(allPlayers[fighter])
+
+        teamA.push(allPlayers[fighter]);
+
         let elementA = document.getElementById(fighter);
         elementA.classList.add('playerA');
-        if(teamA.length == 3){
-            for(let i = 0; i < teamA.length; i++){
-                document.getElementById(teamA[i])
-            };
-        };
+
         team1Assignment();
+
         document.getElementById(fighter).onclick = ''; 
+
     }else if(teamB.length < 3){
         //lo introducimos en la B
-        teamB.push(allPlayers[fighter])
+        teamB.push(allPlayers[fighter]);
 
-        let elementB = document.getElementById(fighter)
+        let elementB = document.getElementById(fighter);
         elementB.classList.add('playerB');
+
         document.getElementById(fighter).onclick = '';
+
         team2Assignment(); 
 
         if(teamB.length == 3){
@@ -104,12 +99,12 @@ let selectFighter = (fighter) => {
                 screenChange('screen1', 'screen2');
                 
             });
-            console.log(showTeamA.innerHTML)
-        }
+        };
 
-    }
-    console.log(p1,p2,p3,p4,p5,p6)  
-}
+    };
+    // console.log(p1,p2,p3,p4,p5,p6)  
+};
+
 
 let team1Assignment = () =>{
     for(let i = 0; i< teamA.length; i++){
@@ -140,35 +135,69 @@ let screenChange = (present,future) =>{
 
 
 let hit = () => {
-    let turn = Math.floor(Math.random() * 5);
-    let special = Math.floor(Math.random() * 8);
-    
+    let turn = Math.floor(Math.random() * 3);
+    let special = Math.floor(Math.random() * 2);
+    let healthPlayer1 = document.getElementById("healthPlayer1");
+    let healthPlayer2 = document.getElementById("healthPlayer2");
+
     if(turn == 0){
         if(special == 3){
             console.log('ATAAAAQUEEEE!')
             p1.specialAttack(p4);
             p2.specialAttack(p5);
             p3.specialAttack(p6);
+            healthPlayer1.value = `${p1.health}`  
+            healthPlayer2.value = `${p1.health}`  
+            healthPlayer3.value = `${p1.health}`  
+
         }else{
+            
             console.log('SPECIAL')
             p1.attack(p4);
             p2.attack(p5);
             p3.attack(p6);
+            healthPlayer1.value = `${p1.health}`  
+            healthPlayer2.value = `${p1.health}`  
+            healthPlayer3.value = `${p1.health}`  
         }
     }else{
         if(special == 3){
             p4.specialAttack(p1);
             p5.specialAttack(p2);
             p6.specialAttack(p3);
+            healthPlayer4.value = `${p1.health}`  
+            healthPlayer5.value = `${p1.health}`  
+            healthPlayer6.value = `${p1.health}`  
+            
         }else{
             p4.attack(p1);
             p5.attack(p2);
             p6.attack(p3);
+            healthPlayer4.value = `${p1.health}`  
+            healthPlayer5.value = `${p1.health}`  
+            healthPlayer6.value = `${p1.health}`  
         }
-    }
 
-    console.log(p1.nick, ':', p1.health);
-    console.log(p2.nick, ':', p2.health);
+    }
+    console.log('VIDA Player1', healthPlayer1)  
+    console.log('VIDA Player2', healthPlayer2)  
+    console.log('VIDA Player3', healthPlayer3)  
+    console.log('VIDA Player4', healthPlayer4)  
+    console.log('VIDA Player5', healthPlayer5)  
+    console.log('VIDA Player6', healthPlayer6)  
+
+    console.log('PRIMERA',p1.nick, ':', Math.floor(p1.health));
+    console.log('<------------------------------------->')
+    console.log('SEGUNDA',p2.nick, ':', Math.floor(p2.health));
+    // console.log('<------------------------------------->')
+    // console.log('TERCERA',p3.nick, ':', Math.floor(p3.health));
+    // console.log('<------------------------------------->')
+    // console.log('CUERTA',p4.nick, ':', Math.floor(p4.health));
+    // console.log('<------------------------------------->')
+    // console.log('QUINTA',p5.nick, ':', Math.floor(p5.health));
+    // console.log('<------------------------------------->')
+    // console.log('SEXTA',p6.nick, ':', Math.floor(p6.health));
+    // console.log('SIGUIENTE RONDA')
 
 }
 
